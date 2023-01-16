f72d41d1d54f881f289483186798924e6cf9b8ff
sachinchauhan4141 <110624213+sachinchauhan4141@users.noreply.github.com>
2023-01-16 14:45:05 +0530

    modified:   src/components/Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,14 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
   const { loginWithRedirect } = useAuth0();
   return (
     <div className="container">
-      <h1 className="my-5">Login To Continue</h1>
+      <h1>Thanks For Visiting...</h1>
+      <h3 className="my-5">Login To Continue...</h3>
+      <h5>Click on the button below..</h5>
       <Link
         onClick={() => loginWithRedirect()}
         className="btn btn-primary mx-1"
